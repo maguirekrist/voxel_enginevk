8f994dbc4cedb9ceb48df6004447227a6af2abf0
Maguire Krist <mkrist@dataoceans.com>
2024-08-27 14:36:04 -0400

    working

--- src/vk_types.h
+++ src/vk_types.h
@@ -3,6 +3,30 @@
 
 #pragma once
 
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
+
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vk_mem_alloc.h>
+
+#include <fmtmsg.h>
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 
 //we will add our main reusable types here
+#define VK_CHECK(x)                                                     \
+    do {                                                                \
+        VkResult err = x;                                               \
+        if (err) {                                                      \
+             fmt::print("Detected Vulkan error: {}", string_VkResult(err)); \
+            abort();                                                    \
+        }                                                               \
+    } while (0)
