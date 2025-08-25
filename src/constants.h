7bc18a90b51f8e275e74da040d7b71693d8fa9c0
Maguire Krist <maguirekrist@gmail.com>
2025-08-25 16:08:41 -0400

    32 view distance

--- src/constants.h
+++ src/constants.h
@@ -12,7 +12,7 @@ constexpr unsigned int TOTAL_BLOCKS_IN_CHUNK = CHUNK_SIZE * CHUNK_SIZE * CHUNK_H
 //number is in chunks away from player position.
 namespace GameConfig
 {
-    constexpr int DEFAULT_VIEW_DISTANCE = 24;
+    constexpr int DEFAULT_VIEW_DISTANCE = 32;
     constexpr int MAXIMUM_CHUNKS = (2 * DEFAULT_VIEW_DISTANCE + 1) * (2 * DEFAULT_VIEW_DISTANCE + 1);
     constexpr float DEFAULT_MOVE_SPEED = 40.0f;
     constexpr float DEFAULT_ROTATION_SPEED = 180.0f;
